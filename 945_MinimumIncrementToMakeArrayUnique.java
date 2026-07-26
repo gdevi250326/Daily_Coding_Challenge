bfbeaa8bc0e7d046116b5bdeee62b1eb29dfe8a0
DEVI G <gdevi250326@gmail.com>
2026-07-26 20:07:13 +0530

    Implement solution for minimum increment to make array unique

---
+++ 945_MinimumIncrementToMakeArrayUnique.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int moves = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                int increment = nums[i - 1] + 1 - nums[i];
+                nums[i] += increment;
+                moves += increment;
+            }
+        }
+
+        return moves;
+    }
+}
