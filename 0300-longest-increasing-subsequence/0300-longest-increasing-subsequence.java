750bdb7225a4c557d2a089bdace1d86735836008
DEVI G <gdevi250326@gmail.com>
2026-08-22 21:06:56 +0530

    Time: 363 ms (15.37%), Space: 236.9 MB (20.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,15 +1,27 @@
-public class Solution {
-    public int lengthOfLIS(int[] nums) {            
-        int[] dp = new int[nums.length];
-        int len = 0;
+class Solution {
+    private int solve(int[] nums, int curr_i, int prev_i, int[][] dp) {
+        if (curr_i == nums.length)
+            return 0;
 
-        for(int x : nums) {
-            int i = Arrays.binarySearch(dp, 0, len, x);
-            if(i < 0) i = -(i + 1);
-            dp[i] = x;
-            if(i == len) len++;
-        }
+        if (dp[curr_i][prev_i] != -1)
+            return dp[curr_i][prev_i];
 
-        return len;
+        int take = 0;
+        if (prev_i == nums.length || nums[prev_i] < nums[curr_i])
+            take = 1 + solve(nums, curr_i + 1, curr_i, dp);
+
+        int skip = solve(nums, curr_i + 1, prev_i, dp);
+
+        return dp[curr_i][prev_i] = Math.max(take, skip);
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 0; i < n + 1; i++)
+            Arrays.fill(dp[i], -1);
+
+        return solve(nums, 0, n, dp);
     }
 }
