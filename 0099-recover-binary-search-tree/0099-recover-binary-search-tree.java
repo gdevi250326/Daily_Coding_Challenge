629923cfa0c7b2ae228cb24d3a112112305372e2
DEVI G <gdevi250326@gmail.com>
2026-08-25 16:27:47 +0530

    Time: 1 ms (100%), Space: 46.8 MB (12.61%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    TreeNode prev = null, first = null, second = null;
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    void inorder(TreeNode root) {
+        if(root == null) return;
+
+        inorder(root.left);
+
+        if(prev != null && root.val < prev.val) {
+            if(first == null) {
+                first = prev;
+            }
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
+        
+    }
+} 
